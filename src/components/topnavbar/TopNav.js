4ba02d08277df1450cca8da42b9707a1ab9e90fa
Mohit200510 <mohit200510@gmail.com>
2026-07-25 20:06:04 +0530

    router issue fixed

--- src/components/topnavbar/TopNav.js
+++ src/components/topnavbar/TopNav.js
@@ -39,22 +39,26 @@ function TopNavBar(){
                         <li id={navItem == 0?"nav-active":""}  data-category="" data-item="0"><a href="">All</a></li>
                         <li id={navItem == 1?"nav-active":""} data-category="Music" data-item= "1"><a href="">Music</a></li>
                         <li id={navItem == 2?"nav-active":""} data-category="Movie trailers" data-item= "2"><a href="">Entertainment</a></li>
-                        <li id={navItem == 3?"nav-active":""} data-category="Music" data-item= "3"><a href="">Star Studios</a></li>
+                        <li id={navItem == 3?"nav-active":""} data-category="Movie trailers" data-item= "3"><a href="">Movie Trailers</a></li>
+                        
                         <li id={navItem == 4?"nav-active":""} data-category="Bhajan" data-item= "4"><a href="">Religious</a></li>
-                        <li id={navItem == 5?"nav-active":""} data-category="Movie trailers" data-item= "5"><a href="">Movie Trailers</a></li>
-                        <li id={navItem == 6?"nav-active":""} data-category="Bhajan" data-item= "6"><a href="">Spiritual</a></li>
-                        <li id={navItem == 7?"nav-active":""} data-category="Music" data-item= "7"><a href="">Songs</a></li>
-                        <li id={navItem == 8?"nav-active":""} data-category="Movie trailers" data-item= "8"><a href="">Masala Films</a></li>
-                        <li id={navItem == 9?"nav-active":""} data-category="Music" data-item= "9"><a href="">Indian pop music</a></li>
-                        <li id={navItem == 10?"nav-active":""} data-category="Bhajan" data-item= "10"><a href="">Spiritual</a></li>
-                        <li id={navItem == 11?"nav-active":""} data-category="Music" data-item= "11"><a href="">New to you</a></li>
-                        <li id={navItem == 12?"nav-active":""} data-category="Movie trailers" data-item= "12"><a href="">Thrillers</a></li>
-                        <li id={navItem == 13?"nav-active":""} data-category="Music" data-item= "13"><a href="">Mixes</a></li>
-                        <i style={{ color: "#f1f1f1" , marginLeft: "6px"}}  className="fa-solid fa-angle-right"></i>
+                        <li id={navItem == 5?"nav-active":""} data-category="Hanuman Chalisa" data-item= "5"><a href="">Hanuman Chalisa</a></li>
+                        <li id={navItem == 6?"nav-active":""} data-category="Music" data-item= "6"><a href="">Star Studios</a></li>
+
+                        <li id={navItem == 7?"nav-active":""} data-category="Bhajan" data-item= "7"><a href="">Spiritual</a></li>
+                        <li id={navItem == 8?"nav-active":""} data-category="Music" data-item= "8"><a href="">Songs</a></li>
+                        <li id={navItem == 9?"nav-active":""} data-category="Movie trailers" data-item= "9"><a href="">Masala Films</a></li>
+                        <li id={navItem == 10?"nav-active":""} data-category="Music" data-item= "10"><a href="">Indian pop music</a></li>
+                        <li id={navItem == 11?"nav-active":""} data-category="Bhajan" data-item= "11"><a href="">Spiritual</a></li>
+                        <li id={navItem == 12?"nav-active":""} data-category="Music" data-item= "12"><a href="">Star Studios</a></li>
+                        <li id={navItem == 13?"nav-active":""} data-category="Music" data-item= "13"><a href="">New to you</a></li>
+                        <li id={navItem == 14?"nav-active":""} data-category="Movie trailers" data-item= "14"><a href="">Thrillers</a></li>
+                        <li id={navItem == 15?"nav-active":""} data-category="Music" data-item= "15"><a href="">Mixes</a></li>
+                        {/* <i style={{ color: "#f1f1f1" , marginLeft: "6px"}}  className="fa-solid fa-angle-right"></i> */}
                         
                     </ul>
 
-                </div>
+        </div>
         </>
     )
 }
